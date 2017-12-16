2f5b374d2fe7e8246e97df9b214cfaafe28dc97f
DDK <DDK@example.com>
2017-12-16 21:36:12 +0300

    update debugging

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -140,7 +140,8 @@ if os.getcwd() == '/app':
 
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    ALLOWED_HOSTS = ['denis-dawn.herokuapp.com',]
+    DEBUG = False
+    ALLOWED_HOSTS = ['denis-dawn.herokuapp.com', ]
 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATICFILES_DIRS = (
